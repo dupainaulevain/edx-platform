a6d52011087fd81373e139ab1b3995d692bbf358
E. Kolpakov <eugeny.kolpakov@gmail.com>
2014-12-02 15:27:00 +0700

    Refactored library handler into dedicated methods for handling various types of requests Switched to require_http_methods instead of explicit checks

--- cms/djangoapps/contentstore/views/library.py
+++ cms/djangoapps/contentstore/views/library.py
@@ -15,6 +15,7 @@ from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
 from django.conf import settings
 from django.utils.translation import ugettext as _, ugettext_noop
+from django.views.decorators.http import require_http_methods
 from django_future.csrf import ensure_csrf_cookie
 from edxmako.shortcuts import render_to_response
 from opaque_keys import InvalidKeyError
@@ -39,6 +40,7 @@ LIBRARIES_ENABLED = settings.FEATURES.get('ENABLE_CONTENT_LIBRARIES', False)
 
 @login_required
 @ensure_csrf_cookie
+@require_http_methods(('GET', 'POST'))
 def library_handler(request, library_key_string=None):
     """
     RESTful interface to most content library related functionality.
@@ -47,44 +49,56 @@ def library_handler(request, library_key_string=None):
         log.exception("Attempted to use the content library API when the libraries feature is disabled.")
         raise Http404  # Should never happen because we test the feature in urls.py also
 
+    if library_key_string is not None and request.method == 'POST':
+        return HttpResponseNotAllowed(("POST",))
+
+    if request.method == 'POST':
+        return _create_library(request)
+
+    # request method is get, since only GET and POST are allowed by @require_http_methods(('GET', 'POST'))
+    if library_key_string:
+        return _display_library(library_key_string, request)
+
+    return _list_libraries(request)
+
+
+def _display_library(library_key_string, request):
+    """
+    Displays single library
+    """
+    library_key = CourseKey.from_string(library_key_string)
+    if not isinstance(library_key, LibraryLocator):
+        log.exception("Non-library key passed to content libraries API.")  # Should never happen due to url regex
+        raise Http404  # This is not a library
+    if not has_course_access(request.user, library_key):
+        log.exception(u"User %s tried to access library %s without permission", request.user.username, unicode(library_key))
+        raise PermissionDenied()
+
+    library = modulestore().get_library(library_key)
+    if library is None:
+        log.exception(u"Library %s not found", unicode(library_key))
+        raise Http404
+
     response_format = 'html'
     if request.REQUEST.get('format', 'html') == 'json' or 'application/json' in request.META.get('HTTP_ACCEPT', 'text/html'):
         response_format = 'json'
 
-    if library_key_string:
-        library_key = CourseKey.from_string(library_key_string)
-        if not isinstance(library_key, LibraryLocator):
-            log.exception("Non-library key passed to content libraries API.")  # Should never happen due to url regex
-            raise Http404  # This is not a library
-        if not has_course_access(request.user, library_key):
-            log.exception(u"User %s tried to access library %s without permission", request.user.username, unicode(library_key))
-            raise PermissionDenied()
+    return library_blocks_view(library, response_format)
 
-        library = modulestore().get_library(library_key)
-        if library is None:
-            log.exception(u"Library %s not found", unicode(library_key))
-            raise Http404
 
-        if request.method == 'GET':
-            return library_blocks_view(library, response_format)
-        return HttpResponseNotAllowed(['GET'])
-
-    elif request.method == 'POST':
-        # Create a new library:
-        return _create_library(request)
-    elif request.method == 'GET':
-        # List all accessible libraries:
-        lib_info = [
-            {
-                "display_name": lib.display_name,
-                "library_key": unicode(lib.location.library_key),
-            }
-            for lib in modulestore().get_libraries()
-            if has_course_access(request.user, lib.location.library_key)
-        ]
-        return JsonResponse(lib_info)
-    else:
-        return HttpResponseNotAllowed(['GET', 'POST'])
+def _list_libraries(request):
+    """
+    List all accessible libraries
+    """
+    lib_info = [
+        {
+            "display_name": lib.display_name,
+            "library_key": unicode(lib.location.library_key),
+        }
+        for lib in modulestore().get_libraries()
+        if has_course_access(request.user, lib.location.library_key)
+    ]
+    return JsonResponse(lib_info)
 
 
 @expect_json
