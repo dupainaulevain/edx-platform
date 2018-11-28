f42668523bdc89161a3b701669c971052df7f659
Guruprasad Lakshmi Narayanan <guruprasad@opencraft.com>
2018-11-28 13:35:59 +0530

    WIP changes for randomizing the order in the course_blocks API

--- lms/djangoapps/course_blocks/transformers/library_content.py
+++ lms/djangoapps/course_blocks/transformers/library_content.py
@@ -98,6 +98,7 @@ class ContentLibraryTransformer(FilteringTransformerMixin, BlockStructureTransfo
 
                 # Save back any changes
                 if any(block_keys[changed] for changed in ('invalid', 'overlimit', 'added')):
+                    state_dict['changed'] = True
                     state_dict['selected'] = list(selected)
                     random.shuffle(state_dict['selected'])
 
@@ -178,3 +179,48 @@ class ContentLibraryTransformer(FilteringTransformerMixin, BlockStructureTransfo
             format_block_keys,
             publish_event,
         )
+
+
+class ContentLibraryRandomizeTransformer(BlockStructureTransformer):
+    """
+    A transformer that manipulates the block structure by randomizing the order of the
+    selected blocks within a library_content module.
+
+    Staff users are *not* exempted from library content pathways/
+    """
+    WRITE_VERSION = 1
+    READ_VERSION = 1
+
+    @classmethod
+    def name(cls):
+        """
+        Unique identifier for the transformer's class;
+        same identifier used in setup.py
+        """
+        return "library_content_randomize"
+
+    @classmethod
+    def collect(cls, block_structure):
+        block_structure.request_xblock_fields('mode')
+        block_structure.request_xblock_fields('max_count')
+
+    def transform(self, usage_info, block_structure):
+        for block_key in block_structure:
+            if block_key.block_type != 'library_content':
+                continue
+
+            state_dict = get_student_module_as_dict(usage_info.user, usage_info.course_key, block_key)
+
+            if 'changed' in state_dict:
+                library_children = block_structure.get_children(block_key)
+                random.shuffle(library_children)
+                state_dict['selected'] = [[child.block_type, child.block_id] for child in library_children]
+                del state_dict['changed']
+                StudentModule.objects.update_or_create(
+                    student=usage_info.user,
+                    course_id=usage_info.course_key,
+                    module_state_key=block_key,
+                    defaults={
+                        'state': json.dumps(state_dict),
+                    }
+                )
