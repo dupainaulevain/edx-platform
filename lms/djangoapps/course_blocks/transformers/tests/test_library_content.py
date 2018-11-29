4d958290b05aeb59e12424ea7233f2f279b370f4
Guruprasad Lakshmi Narayanan <guruprasad@opencraft.com>
2018-11-29 20:55:52 +0530

    Add unit tests

--- lms/djangoapps/course_blocks/transformers/tests/test_library_content.py
+++ lms/djangoapps/course_blocks/transformers/tests/test_library_content.py
@@ -1,13 +1,14 @@
 """
 Tests for ContentLibraryTransformer.
 """
+import mock
 
 from openedx.core.djangoapps.content.block_structure.api import clear_course_from_cache
 from openedx.core.djangoapps.content.block_structure.transformers import BlockStructureTransformers
 from student.tests.factories import CourseEnrollmentFactory
 
 from ...api import get_course_blocks
-from ..library_content import ContentLibraryTransformer
+from ..library_content import ContentLibraryTransformer, ContentLibraryRandomizeTransformer
 from .helpers import CourseStructureTestCase
 
 
@@ -180,3 +181,132 @@ class ContentLibraryTransformerTestCase(CourseStructureTestCase):
             transformers=self.transformers
         )
         self.assertEqual(len(list(transformed_blocks.get_block_keys())), len(self.blocks))
+
+
+class ContentLibraryRandomizeTransformerTestCase(CourseStructureTestCase):
+    TRANSFORMER_CLASS_TO_TEST = ContentLibraryRandomizeTransformer
+
+    def setUp(self):
+        super(ContentLibraryRandomizeTransformerTestCase, self).setUp()
+        self.course_hierarchy = self.get_course_hierarchy()
+        self.blocks = self.build_course(self.course_hierarchy)
+        self.course = self.blocks['course']
+        clear_course_from_cache(self.course.id)
+
+        # Enroll user in course.
+        CourseEnrollmentFactory.create(user=self.user, course_id=self.course.id, is_active=True)
+
+    def get_course_hierarchy(self):
+        """
+        Get a course hierarchy to test with.
+        """
+        return [{
+            'org': 'ContentLibraryTransformer',
+            'course': 'CL101F',
+            'run': 'test_run',
+            '#type': 'course',
+            '#ref': 'course',
+            '#children': [
+                {
+                    '#type': 'chapter',
+                    '#ref': 'chapter1',
+                    '#children': [
+                        {
+                            '#type': 'sequential',
+                            '#ref': 'lesson1',
+                            '#children': [
+                                {
+                                    '#type': 'vertical',
+                                    '#ref': 'vertical1',
+                                    '#children': [
+                                        {
+                                            'metadata': {'category': 'library_content'},
+                                            '#type': 'library_content',
+                                            '#ref': 'library_content1',
+                                            '#children': [
+                                                {
+                                                    'metadata': {'display_name': "CL Vertical 2"},
+                                                    '#type': 'vertical',
+                                                    '#ref': 'vertical2',
+                                                    '#children': [
+                                                        {
+                                                            'metadata': {'display_name': "HTML1"},
+                                                            '#type': 'html',
+                                                            '#ref': 'html1',
+                                                        }
+                                                    ]
+                                                },
+                                                {
+                                                    'metadata': {'display_name': "CL Vertical 3"},
+                                                    '#type': 'vertical',
+                                                    '#ref': 'vertical3',
+                                                    '#children': [
+                                                        {
+                                                            'metadata': {'display_name': "HTML2"},
+                                                            '#type': 'html',
+                                                            '#ref': 'html2',
+                                                        }
+                                                    ]
+                                                },
+                                                {
+                                                    'metadata': {'display_name': "CL Vertical 4"},
+                                                    '#type': 'vertical',
+                                                    '#ref': 'vertical4',
+                                                    '#children': [
+                                                        {
+                                                            'metadata': {'display_name': "HTML3"},
+                                                            '#type': 'html',
+                                                            '#ref': 'html3',
+                                                        }
+                                                    ]
+                                                }
+                                            ]
+                                        }
+                                    ],
+                                }
+                            ],
+                        }
+                    ],
+                }
+            ]
+        }]
+
+    @mock.patch('lms.djangoapps.course_blocks.transformers.library_content.get_student_module_as_dict')
+    def test_content_library_randomize(self, mocked):
+        """
+        Test when course has content library section.
+        First test user can't see any content library section,
+        and after that mock response from MySQL db.
+        Check user can see mocked sections in content library.
+        """
+        mocked.return_value = {
+            'selected': [
+                ['vertical', 'vertical_vertical3'],
+                ['vertical', 'vertical_vertical2'],
+                ['vertical', 'vertical_vertical4'],
+            ]
+        }
+        for i in range(5):
+            trans_block_structure = get_course_blocks(
+                self.user,
+                self.course.location,
+                self.transformers,
+            )
+            self.assertEqual(
+                set(trans_block_structure.get_block_keys()),
+                self.get_block_key_set(
+                    self.blocks,
+                    'course',
+                    'chapter1',
+                    'lesson1',
+                    'vertical1',
+                    'library_content1',
+                    'vertical3',
+                    'html2',
+                    'vertical2',
+                    'html1',
+                    'vertical4',
+                    'html3'
+                ),
+                "Expected 'selected' equality failed in iteration {}.".format(i)
+            )
